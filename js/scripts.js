a7fcb998351076a93a886b86d08de6fb32d9101a
Nicholas Leung <63422690+natanleung@users.noreply.github.com>
2020-05-02 22:02:32 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Import background (Vanta.js)
+VANTA.WAVES({
+    el: "*",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xa0c0e,
+    waveHeight: 40.00,
+    waveSpeed: 0.25
+})
+
+document.body.style.background = VANTA.WAVES;
+document.body.style.backgroundAttachment = "fixed";
